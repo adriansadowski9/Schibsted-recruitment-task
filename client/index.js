63463822724146b5b15f492741769c636c86560e
luvisoniasty <luvisonx@gmail.com>
2019-06-09 16:21:48 +0200

    Revert "getting data from api"
    This reverts commit aeb280ba3174a1ba083d388d6179054d20ae5d18.

--- client/index.js
+++ client/index.js
@@ -1,49 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            gifs: {},
-            images: {},
-            searchQuery: '',
-            page: 1
-        }
-    }
-
-    getGifs() {
-        axios.get('/api/gifs', {
-            params: {
-                search: this.state.searchQuery,
-                page: this.state.page
-            }
-        })
-        .then((res) => this.setState({gifs: res.data.data}))
-        .catch((error) => console.log(error));
-    }
-
-    getImages() {
-        axios.get('/api/images', {
-            params: {
-                search: this.state.searchQuery,
-                page: this.state.page
-            }
-        })
-        .then((res) => this.setState({images: res.data.hits}))
-        .catch((error) => console.log(error));
-    }
-
-    componentDidMount() {
-        this.getGifs();
-        this.getImages();
-    }
-
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <h1>Hello World6</h1>
             </div>
         )
     }
--- server.js
+++ server.js
@@ -1,65 +1,11 @@
-const express = require('express'),
-      app = express(),
-      axios = require('axios'),
-      path = require('path');
+const express = require("express"),
+      app = express();
 
 app.use(express.static('public'));
 
-app.get('/api/gifs', (req, res) => {
-    if(req.query.search) {
-        const offset = (req.query.page - 1) * 20;
-        axios.get(`https://api.giphy.com/v1/gifs/search`, {
-            params: {
-                api_key: 'qwhv3ycc9rLMjy5J9rlXkaQEtlaZzRBW',
-                q: req.query.search,
-                limit: 20,
-                offset: offset
-            }
-        })
-        .then((response) => res.json(response.data))
-        .catch((error) => res.json(error));
-    }
-    else {
-        const offset = (req.query.page - 1) * 20;
-        axios.get('https://api.giphy.com/v1/gifs/trending', {
-            params: {
-                api_key: 'qwhv3ycc9rLMjy5J9rlXkaQEtlaZzRBW',
-                limit: 20,
-                offset: offset
-            }
-        })
-        .then((response) => res.json(response.data))
-        .catch((error) => res.json(error));
-    }
+app.get('/', (req, res) => {
+    res.send('hello');
 });
 
-app.get('/api/images', (req, res) => {
-    if(req.query.search) {
-        axios.get(`https://pixabay.com/api/`, {
-            params: {
-                key: '7715054-438327f2e182ffe2d984b22e4',
-                q: req.query.search,
-                page: req.query.page
-            }
-        })
-        .then((response) => res.json(response.data))
-        .catch((error) => res.json(error));
-    }
-    else {
-        axios.get('https://pixabay.com/api/', {
-            params: {
-                key: '7715054-438327f2e182ffe2d984b22e4',
-                page: req.query.page
-            }
-        })
-        .then((response) => res.json(response.data))
-        .catch((error) => res.json(error));
-    }
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port,  () => console.log(`App listening on port ${port}`));
